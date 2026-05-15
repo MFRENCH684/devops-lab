371098378f77dde839665d7768a4fc935602e98f
Matthew French <your-actual-github-email@email.com>
2026-05-15 08:42:02 -0700

    Add remote state backend with S3 and state locking

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -5,7 +5,15 @@ terraform {
    version = "~> 5.0"
   }
  }
-}
+
+ backend "s3" {
+   bucket = "devops-lab-tfstate-237575223274"
+   key = "vpc/terraform.tfstate"
+   region = "us-east-1"
+   use_lockfile = true
+   encrypt = true
+  }
+ }
 
 provider "aws" {
  region = var.aws_region
